8237c9c0e779d529b1a1137a36a175728d1a3f49
Mauricio Bonetti <bonetti.mauricio@gmail.com>
2017-01-26 17:24:03 +0000

    First commit

---
+++ app/components/Main.js
@@ -0,0 +1,9 @@
+var React = require("react");
+
+var Main = React.createClass({
+  render: function() {
+    return (<div>{this.props.children}</div>)
+  }
+})
+
+module.exports = Main;
---
+++ app/index.js
@@ -0,0 +1,6 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+var Routes = require("./config/routes");
+
+ReactDOM.render(Routes, document.getElementById("app"));
+
